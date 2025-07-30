bdb91c225708dabdc857b36e2e06a6e82a918549
AvitalSt <154595696+AvitalSt@users.noreply.github.com>
2025-07-30 11:58:55 +0300

    update styling

--- src/pages/TasksPage.tsx
+++ src/pages/TasksPage.tsx
@@ -6,7 +6,16 @@ import { useNavigate } from "react-router-dom";
 import ClipLoader from "react-spinners/ClipLoader";
 
 export default function TasksPage() {
-  const {tasks,filter,setFilter,loading,error,applyFilter,handleDelete,handleToggleComplete,} = useTasks();
+  const {
+    tasks,
+    filter,
+    setFilter,
+    loading,
+    error,
+    applyFilter,
+    handleDelete,
+    handleToggleComplete,
+  } = useTasks();
   const navigate = useNavigate();
 
   return (
@@ -15,30 +24,37 @@ export default function TasksPage() {
         Add task
       </button>
 
-      {loading && (
-        <div style={{ textAlign: "center", padding: "1rem" }}>
-          <ClipLoader color="#7cb8f9" loading={loading} size={50} />
-        </div>
-      )}
-
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      {!loading && !error && tasks.length === 0 && <p>No tasks to display</p>}
-
       <h1>Task management</h1>
 
       <TaskFilters
         filter={filter}
-        onFilterChange={(newFilter) => setFilter((prev) => ({ ...prev, ...newFilter }))}
+        onFilterChange={(newFilter) =>
+          setFilter((prev) => ({ ...prev, ...newFilter }))
+        }
         onApplyFilter={applyFilter}
       />
 
-      {!loading && !error && tasks.length > 0 && (
-        <TaskList
-          tasks={tasks}
-          onDelete={handleDelete}
-          onEdit={(task) => navigate(`/edit/${task.id}`)}
-          onToggleComplete={handleToggleComplete}
-        />
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      {!loading && !error && tasks.length === 0 && (
+        <div className="no-tasks-message">No tasks to display</div>
+      )}
+
+      {!error && (
+        <div className="task-list-wrapper">
+          {loading ? (
+            <div className="loading-spinner">
+              <ClipLoader color="#7cb8f9" loading={true} size={50} />
+            </div>
+          ) : (
+            <TaskList
+              tasks={tasks}
+              onDelete={handleDelete}
+              onEdit={(task) => navigate(`/edit/${task.id}`)}
+              onToggleComplete={handleToggleComplete}
+            />
+          )}
+        </div>
       )}
     </div>
   );
